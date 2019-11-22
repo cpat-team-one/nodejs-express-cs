ff4dc2980728a6bce7e8bdab9c41bd6a23a54c7b
Carlos Santana <csantana23@gmail.com>
2019-11-22 17:21:41 -0500

    lets try again

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from IBM Cloud Pak for Applications");
+  res.send("Hello from IBM Cloud Pak for Applications with pipeline fix");
 });
 
 module.exports.app = app;
