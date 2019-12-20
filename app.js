dc71202b4ddf5623fb4b7540a7913f967fcb1bfb
Carlos Santana <csantanapr@apache.org>
2019-12-20 15:54:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from IBM Cloud Pak for Applications mooc-ocp-three");
+  res.send("Hello from IBM Cloud Pak for Applications mooc-ocp-cpat");
 });
 
 module.exports.app = app;
